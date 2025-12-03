3287e7208e48fedaae5e557d5a1d0487fdd62918
megu01s <tyrx1@outlook.fr>
2025-12-03 11:51:57 +0100

    Estado inicial del proyecto

---
+++ src/main/java/com/example/CrudEmpleadosSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmpleadosSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmpleadosSpringMvcApplication.class, args);
+	}
+
+}
