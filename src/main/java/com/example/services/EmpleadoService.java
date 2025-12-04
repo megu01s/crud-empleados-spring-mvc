8141b570b6f5993adeec1595fd8a21cf1485abef
megu01s <tyrx1@outlook.fr>
2025-12-04 17:11:26 +0100

    add DepartamentoServiceImpl and EmpleadoServiceImpl with basic CRUD operations

--- src/main/java/com/example/services/EmpleadoService.java
+++ src/main/java/com/example/services/EmpleadoService.java
@@ -7,8 +7,8 @@ import com.example.entities.Empleado;
 public interface EmpleadoService {
 
     List<Empleado> getAllEmpleados();
-    Empleado getEmpleado();
-    Empleado saveEmpleado();
+    Empleado getEmpleado(int empleadoId);
+    Empleado saveEmpleado(Empleado empleado);
     void deleteEmpleado(Empleado empleado);
     Empleado updateEmpleado(int empleadoId);
     List<Empleado> findByNombre(String nombre);
